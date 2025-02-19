af82674ba3dd8fde3bc73dd608da76825100d646
RaghadNasman <raghadnasman9@gmail.com>
2025-02-19 02:33:21 +0200

    third commit

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -32,6 +32,31 @@
     <div class="container mt-4">
         <div class="offset-md-2 col-md-8">
             <div class="card">
+                @if (isset($task))
+                <div class="card-header">
+                    Update Task
+                </div>
+                <div class="card-body">
+                    <!-- Update Task Form -->
+                    <form action="{{url('update')}}" method="POST">
+                        @csrf
+                        {{-- لتمرير قيمة id --}}
+                        <input type="hidden" value="{{$task->id}}" name="id">
+                        <!-- Task Name -->
+                        <div class="mb-3">
+                            <label for="task-name" class="form-label">Task</label>
+                            <input type="text" name="name" id="task-name" class="form-control" value="{{$task->name}}">
+                        </div>
+
+                        <!-- Update Task Button -->
+                        <div>
+                            <button type="submit" class="btn btn-primary">
+                                <i class="fa fa-plus me-2"></i>Update Task
+                            </button>
+                        </div>
+                    </form>
+                </div>
+                @else
                 <div class="card-header">
                     New Task
                 </div>
@@ -53,6 +78,7 @@
                         </div>
                     </form>
                 </div>
+                @endif
             </div>
 
             <!-- Current Tasks -->
@@ -69,17 +95,26 @@
                             </tr>
                         </thead>
                         <tbody>
+                            @foreach ($tasks as $task)
                             <tr>
-                                <td>Task 1</td>
+                                <td>{{$task -> name}}</td>
                                 <td>
-                                    <form action="#" method="POST" class="d-inline">
+                                    <form action="/delete/{{$task->id}}" method="POST" class="d-inline">
+                                        @csrf
                                         <button type="submit" class="btn btn-danger">
                                             <i class="fa fa-trash me-2"></i>Delete
                                         </button>
                                     </form>
+                                    <form action="/edit/{{$task->id}}" method="POST" class="d-inline">
+                                        @csrf
+                                        <button type="submit" class="btn btn-info">
+                                            <i class="fa fa-info me-2"></i>Edit
+                                        </button>
+                                    </form>
                                 </td>
                             </tr>
-                            <tr>
+                            @endforeach
+                            {{-- <tr>
                                 <td>Task 2</td>
                                 <td>
                                     <form action="#" method="POST" class="d-inline">
@@ -98,7 +133,7 @@
                                         </button>
                                     </form>
                                 </td>
-                            </tr>
+                            </tr> --}}
                         </tbody>
                     </table>
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -13,30 +13,54 @@ Route::get('/about', function () {
         '1' => 'Tichnical',
         '2' => 'Financial',
         '3' => 'Sales',
-       ];
+    ];
     // return  view('about') ->with(key: 'name',value: $name);
     // return  View(view:'about', data:['name' => $name]) ;
     // return  View(view:'about', data:compact('name')) ;
-    return  View(view:'about', data:compact('name', 'departments')) ;
+    return  View(view: 'about', data: compact('name', 'departments'));
 });
 //when submit about form
 Route::post('/about', function () {
     // Global array to recieve information from form
-   $name =  $_POST['name'];//value of name attribute in input
-   $departments = [
-    '1' => 'Tichnical',
-    '2' => 'Financial',
-    '3' => 'Sales',
-   ];
-    return  View(view:'about', data:compact('name', 'departments')) ;
+    $name =  $_POST['name']; //value of name attribute in input
+    $departments = [
+        '1' => 'Tichnical',
+        '2' => 'Financial',
+        '3' => 'Sales',
+    ];
+    return  View(view: 'about', data: compact('name', 'departments'));
 });
 
 //tasks view
-Route::get('tasks', function (){
-    return View(view:'tasks');
+Route::get('tasks', function () {
+    $tasks = DB::table('tasks')->get();
+    return View(view: 'tasks', data: compact('tasks'));
 });
-Route::post('create', function (){
+// add task
+Route::post('create', function () {
     $task_name = $_POST['name'];
-    DB::table(table: 'tasks')-> insert(values: ['name' => $task_name]);
-    return view('tasks');
+    DB::table(table: 'tasks')->insert(values: ['name' => $task_name]);
+    // return view('tasks'); couses error
+    return redirect()->back();
+});
+
+// delete task
+Route::post('delete/{id}' , function ($id) {
+    // DB::table('tasks') ->where(column: 'id' , operator: '=' , value: $id);
+    DB::table('tasks')->where(column: 'id', operator: $id)->delete();
+    return redirect()->back();
+});
+
+//edit task
+Route::post('edit/{id}' , function ($id) {
+    $task =DB::table('tasks')->where('id',$id)->first();
+    $tasks = DB::table('tasks')->get();
+    return view('tasks',compact('task', 'tasks'));
+});
+
+//update task
+Route::post('update',function () {
+    $id = $_POST['id'];
+    DB::table('tasks')->where('id' ,'=',$id)->update(['name' => $_POST['name']]);
+    return redirect(to: 'tasks');
 });
