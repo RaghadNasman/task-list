4384db110a3678c2061b651c10b09004f50ffb42
RaghadNasman <raghadnasman9@gmail.com>
2025-02-22 10:49:15 +0200

    fourth commit

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,51 +1,70 @@
 @extends('layouts.app')
-@section(section: 'content')
+@section(section: 'usercontent')
     <div class="container mt-4">
-        <h1>Task List App</h1>
+        <h1>Users</h1>
         <div class="offset-md-2 col-md-8">
             <div class="card">
-                @if (isset($task))
+                @if (isset($user))
                 <div class="card-header">
-                    Update Task
+                    Update User
                 </div>
                 <div class="card-body">
-                    <!-- Update Task Form -->
+                    <!-- Update User Form -->
                     <form action="{{url('update')}}" method="POST">
                         @csrf
-                        {{-- لتمرير قيمة id --}}
-                        <input type="hidden" value="{{$task->id}}" name="id">
-                        <!-- Task Name -->
+                        <input type="hidden" value="{{$user->id}}" name="id">
+                        <!-- User Name -->
                         <div class="mb-3">
-                            <label for="task-name" class="form-label">Task</label>
-                            <input type="text" name="name" id="task-name" class="form-control" value="{{$task->name}}">
+                            <label for="user-name" class="form-label">Name</label>
+                            <input type="text" name="username" id="user-name" class="form-control" value="{{$user->name}}">
+                        </div>
+                        <!-- User Name -->
+                        <div class="mb-3">
+                            <label for="user-email" class="form-label">Email</label>
+                            <input type="email" name="email" id="user-email" class="form-control" value="{{$user->email}}">
+                        </div>
+                        <!-- User Name -->
+                        <div class="mb-3">
+                            <label for="user-password" class="form-label">Password</label>
+                            <input type="password" name="password" id="user-password" class="form-control" value="{{$user->password}}">
                         </div>
 
                         <!-- Update Task Button -->
                         <div>
                             <button type="submit" class="btn btn-primary">
-                                <i class="fa fa-plus me-2"></i>Update Task
+                                <i class="fa fa-plus me-2"></i>Update User
                             </button>
                         </div>
                     </form>
                 </div>
                 @else
                 <div class="card-header">
-                    New Task
+                    New User
                 </div>
                 <div class="card-body">
-                    <!-- New Task Form -->
+                    <!-- New User Form -->
                     <form action="create" method="POST">
                         @csrf
-                        <!-- Task Name -->
+                        <!-- User Name -->
                         <div class="mb-3">
-                            <label for="task-name" class="form-label">Task</label>
-                            <input type="text" name="name" id="task-name" class="form-control" value="">
+                            <label for="user-name" class="form-label">Name</label>
+                            <input type="text" name="username" id="user-name" class="form-control" value="">
+                        </div>
+                        <!-- User Name -->
+                        <div class="mb-3">
+                            <label for="user-email" class="form-label">Email</label>
+                            <input type="email" name="email" id="user-email" class="form-control" value="">
+                        </div>
+                        <!-- User Name -->
+                        <div class="mb-3">
+                            <label for="user-password" class="form-label">Password</label>
+                            <input type="password" name="password" id="user-password" class="form-control" value="">
                         </div>
 
                         <!-- Add Task Button -->
                         <div>
                             <button type="submit" class="btn btn-primary">
-                                <i class="fa fa-plus me-2"></i>Add Task
+                                <i class="fa fa-plus me-2"></i>Add User
                             </button>
                         </div>
                     </form>
@@ -56,28 +75,28 @@
             <!-- Current Tasks -->
             <div class="card mt-4">
                 <div class="card-header">
-                    Current Tasks
+                    Current Users
                 </div>
                 <div class="card-body">
                     <table class="table table-striped">
                         <thead>
                             <tr>
-                                <th>Task</th>
+                                <th>User</th>
                                 <th>Actions</th>
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach ($tasks as $task)
+                            @foreach ($users as $user )
                             <tr>
-                                <td>{{$task -> name}}</td>
+                                <td>{{$user -> name}}</td>
                                 <td>
-                                    <form action="/delete/{{$task->id}}" method="POST" class="d-inline">
+                                    <form action="/delete/{{$user->id}}" method="POST" class="d-inline">
                                         @csrf
                                         <button type="submit" class="btn btn-danger">
                                             <i class="fa fa-trash me-2"></i>Delete
                                         </button>
                                     </form>
-                                    <form action="/edit/{{$task->id}}" method="POST" class="d-inline">
+                                    <form action="/edit/{{$user->id}}" method="POST" class="d-inline">
                                         @csrf
                                         <button type="submit" class="btn btn-info">
                                             <i class="fa fa-info me-2"></i>Edit
@@ -86,26 +105,8 @@
                                 </td>
                             </tr>
                             @endforeach
-                            {{-- <tr>
-                                <td>Task 2</td>
-                                <td>
-                                    <form action="#" method="POST" class="d-inline">
-                                        <button type="submit" class="btn btn-danger">
-                                            <i class="fa fa-trash me-2"></i>Delete
-                                        </button>
-                                    </form>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Task 3</td>
-                                <td>
-                                    <form action="#" method="POST" class="d-inline">
-                                        <button type="submit" class="btn btn-danger">
-                                            <i class="fa fa-trash me-2"></i>Delete
-                                        </button>
-                                    </form>
-                                </td>
-                            </tr> --}}
+
+
                         </tbody>
                     </table>
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -32,35 +34,54 @@ Route::post('/about', function () {
 });
 
 //tasks view
-Route::get('tasks', function () {
-    $tasks = DB::table('tasks')->get();
-    return View(view: 'tasks', data: compact('tasks'));
-});
+// Route::get('tasks', function () {
+//     $tasks = DB::table('tasks')->get();
+//     return View(view: 'tasks', data: compact('tasks'));
+// });
+Route::get('tasks', [TaskController::class, 'index']);
 // add task
-Route::post('create', function () {
-    $task_name = $_POST['name'];
-    DB::table(table: 'tasks')->insert(values: ['name' => $task_name]);
-    // return view('tasks'); couses error
-    return redirect()->back();
-});
+// Route::post('create', function () {
+//     $task_name = $_POST['name'];
+//     DB::table(table: 'tasks')->insert(values: ['name' => $task_name]);
+//     // return view('tasks'); couses error
+//     return redirect()->back();
+// });
+Route::post('create', [TaskController::class, 'create']);
 
 // delete task
-Route::post('delete/{id}' , function ($id) {
-    // DB::table('tasks') ->where(column: 'id' , operator: '=' , value: $id);
-    DB::table('tasks')->where(column: 'id', operator: $id)->delete();
-    return redirect()->back();
-});
+// Route::post('delete/{id}' , function ($id) {
+//     // DB::table('tasks') ->where(column: 'id' , operator: '=' , value: $id);
+//     DB::table('tasks')->where(column: 'id', operator: $id)->delete();
+//     return redirect()->back();
+// });
+Route::post('delete/{id}' , [TaskController::class, 'destroy'] );
 
 //edit task
-Route::post('edit/{id}' , function ($id) {
-    $task =DB::table('tasks')->where('id',$id)->first();
-    $tasks = DB::table('tasks')->get();
-    return view('tasks',compact('task', 'tasks'));
-});
+// Route::post('edit/{id}' , function ($id) {
+//     $task =DB::table('tasks')->where('id',$id)->first();
+//     $tasks = DB::table('tasks')->get();
+//     return view('tasks',compact('task', 'tasks'));
+// });
+Route::post('edit/{id}' , [TaskController::class, 'edit']);
 
 //update task
-Route::post('update',function () {
-    $id = $_POST['id'];
-    DB::table('tasks')->where('id' ,'=',$id)->update(['name' => $_POST['name']]);
-    return redirect(to: 'tasks');
+// Route::post('update',function () {
+//     $id = $_POST['id'];
+//     DB::table('tasks')->where('id' ,'=',$id)->update(['name' => $_POST['name']]);
+//     return redirect(to: 'tasks');
+// });
+Route::post('update', [TaskController::class, 'update']);
+
+
+// display layaout
+Route::get('app', function () {
+    return view ('layouts.app');
 });
+
+// user Routes
+Route::get('users', [UserController::class, 'index']);
+Route::post('create', [UserController::class, 'create']);
+Route::post('delete/{id}', [UserController::class, 'destroy']);
+Route::post('edit/{id}', [UserController::class, 'edit']);
+Route::post('update', [UserController::class, 'update']);
+
